ce8cf49faf9e774bca37abdbbe6add9bde610c6a
Jerry Nixon <jerry@nixoncorp.com>
2017-10-17 13:53:30 -0600

    End of Module 01

---
+++ OneDayWorkshop01/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace OneDayWorkshop01
+{
+    sealed partial class App : Application
+    {
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            Window.Current.Content = new Views.ShellPage();
+            Window.Current.Activate();
+        }
+    }
+}
---
+++ OneDayWorkshop01/Views/ShellPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace OneDayWorkshop01.Views
+{
+    public sealed partial class ShellPage : Page
+    {
+        public ShellPage()
+        {
+            InitializeComponent();
+            NavigationFrame.Navigated += NavigationFrame_Navigated;
+            SystemNavigationManager.GetForCurrentView().BackRequested += ShellPage_BackRequested;
+        }
+
+        private void ShellPage_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            NavigationFrame.GoBack();
+        }
+
+        private void NavigationFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+            if (NavigationFrame.CanGoBack)
+            {
+                SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility
+                    = AppViewBackButtonVisibility.Visible;
+            }
+            else
+            {
+                SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility
+                    = AppViewBackButtonVisibility.Collapsed;
+            }
+            switch (NavigationFrame.Content)
+            {
+                case CodePage p:
+                    MainNavigationView.SelectedItem = CodeButton;
+                    break;
+                case PullPage p:
+                    MainNavigationView.SelectedItem = PullButton;
+                    break;
+                case SettingsPage p:
+                    MainNavigationView.SelectedItem = MainNavigationView.SettingsItem;
+                    break;
+                case IssuesPage p:
+                    MainNavigationView.SelectedItem = IssuesButton;
+                    break;
+            }
+        }
+
+        private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.IsSettingsSelected && !(NavigationFrame.Content is SettingsPage))
+            {
+                NavigationFrame.Navigate(typeof(SettingsPage));
+            }
+            else
+            {
+                switch ((args.SelectedItem as NavigationViewItem).Name)
+                {
+                    case nameof(CodeButton) when (!(NavigationFrame.Content is CodePage)):
+                        NavigationFrame.Navigate(typeof(CodePage));
+                        break;
+                    case nameof(IssuesButton) when (!(NavigationFrame.Content is IssuesPage)):
+                        NavigationFrame.Navigate(typeof(IssuesPage));
+                        break;
+                    case nameof(PullButton) when (!(NavigationFrame.Content is PullPage)):
+                        NavigationFrame.Navigate(typeof(PullPage));
+                        break;
+                }
+            }
+            NavigationFrame.BackStack.Clear();
+        }
+    }
+}
