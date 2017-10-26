bbef51727b20c72fa683ca2c387191eb5ea157ff
Daren May <daren.may@gmail.com>
2017-10-26 15:47:00 -0600

    Starting Module4

---
+++ Modules/Module4/2 - End Solution/GitHubBrowser/GitHubBrowser/ViewModels/SettingsPageViewModel.cs
@@ -0,0 +1,48 @@
+﻿using GalaSoft.MvvmLight.Command;
+using GitHubBrowser.Services;
+using Windows.ApplicationModel;
+
+namespace GitHubBrowser.ViewModels
+{
+    class SettingsPageViewModel
+    {
+        private SettingsService _settingService;
+        private GitHubService _githubService;
+
+        public SettingsPageViewModel()
+        {
+            if (Windows.ApplicationModel.DesignMode.DesignModeEnabled
+                || Windows.ApplicationModel.DesignMode.DesignMode2Enabled)
+            {
+                return;
+            }
+            _settingService = new SettingsService();
+            _githubService = new GitHubService();
+        }
+
+        public RelayCommand _clearDefaultCommand;
+        public RelayCommand ClearDefaultCommand
+        {
+            get => _clearDefaultCommand ?? (_clearDefaultCommand = new RelayCommand(ClearDefaultExecute, ClearDefaultCanExecute));
+        }
+        private void ClearDefaultExecute()
+        {
+            _settingService.DefaultRepository = null;
+            ClearDefaultCommand.RaiseCanExecuteChanged();
+        }
+        private bool ClearDefaultCanExecute()
+        {
+            return !string.IsNullOrEmpty(_settingService.DefaultRepository);
+        }
+
+        public string Version
+        {
+            get
+            {
+                var p = Package.Current;
+                var v = p.Id.Version;
+                return $"{p.DisplayName} v{v.Major}.{v.Minor}.{v.Build}.{v.Revision}";
+            }
+        }
+    }
+}
